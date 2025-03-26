a9a3d917dd327f929ea94ff48b748110bef899f8
Maarten Goossens <104869228+m01g@users.noreply.github.com>
2025-03-26 20:39:54 +0100

    Update bliynktrein.ino

--- bliynktrein.ino
+++ bliynktrein.ino
@@ -10,7 +10,7 @@
 //wifi instelingen
 char auth[] = "lbQNxJsI_j5gAbk4G_0Z75_3jKD0jYc8";
 char ssid[] = "***REMOVED***";
-char pass[] = "***REMOVED***";
+char pass[] = "";
 
 // MQTT-instellingen
 const char* mqttServer = "192.168.1.42";
